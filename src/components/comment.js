593e9ce8152f0f95f4c556fa719e729e3026c3f7
Анна Кузнецова <akimova_anna_25@mail.ru>
2020-04-13 00:19:34 +0300

    Рефакторинг 'comment.js'.

--- src/components/comment.js
+++ src/components/comment.js
@@ -1,27 +1,19 @@
 import {formatDateWithSlash, formatTime} from "../utils.js";
+import {EMOJI} from "../const.js";
 
 const createEmojiListMarkup = (emojis) => {
   return (
     `<div class="film-details__emoji-list">
-      <input class="film-details__emoji-item visually-hidden" name="comment-emoji" type="radio" id="emoji-smile" value="smile">
-      <label class="film-details__emoji-label" for="emoji-smile">
-        <img src="./images/emoji/smile.png" width="30" height="30" alt="emoji">
-      </label>
-
-      <input class="film-details__emoji-item visually-hidden" name="comment-emoji" type="radio" id="emoji-sleeping" value="sleeping">
-      <label class="film-details__emoji-label" for="emoji-sleeping">
-        <img src="./images/emoji/sleeping.png" width="30" height="30" alt="emoji">
-      </label>
-
-      <input class="film-details__emoji-item visually-hidden" name="comment-emoji" type="radio" id="emoji-puke" value="puke">
-      <label class="film-details__emoji-label" for="emoji-puke">
-        <img src="./images/emoji/puke.png" width="30" height="30" alt="emoji">
-      </label>
-
-      <input class="film-details__emoji-item visually-hidden" name="comment-emoji" type="radio" id="emoji-angry" value="angry">
-      <label class="film-details__emoji-label" for="emoji-angry">
-        <img src="./images/emoji/angry.png" width="30" height="30" alt="emoji">
-      </label>
+      ${Array.from(emojis)
+          .map(([emojiTitle, emojiUrl]) => {
+            return (
+              `<input class="film-details__emoji-item visually-hidden" name="comment-emoji" type="radio" id="emoji-${emojiTitle}" value="${emojiTitle}">
+              <label class="film-details__emoji-label" for="emoji-${emojiTitle}">
+                <img src="./images/emoji/${emojiUrl}" width="30" height="30" alt="emoji">
+              </label>`
+            );
+          })
+          .join(`\n`)}
     </div>`
   );
 };
@@ -67,7 +59,7 @@ const createCommentsTemplate = (comments) => {
           <textarea class="film-details__comment-input" placeholder="Select reaction below and write comment here" name="comment"></textarea>
         </label>
 
-        ${createEmojiListMarkup()}
+        ${createEmojiListMarkup(EMOJI)}
       </div>
     </section>`
   );
