5cbd128a4eba5b4764d806e4c1e58abe13ccb558
Анна Кузнецова <akimova_anna_25@mail.ru>
2020-04-13 03:26:26 +0300

    Добавление клсса-названия для блоков 'films-list--extra'.

--- src/components/films-list-extra.js
+++ src/components/films-list-extra.js
@@ -1,8 +1,17 @@
-export const createFilmsListExtraTemplate = (title) => {
+const getClassName = (title) => {
+  return title.toLowerCase()
+    .split(` `)
+    .join(`-`);
+};
+
+const createFilmsListExtraTemplate = (title) => {
   return (
-    `<section class="films-list--extra">
+    `<section class="films-list--extra ${getClassName(title)}">
       <h2 class="films-list__title">${title}</h2>
       <div class="films-list__container"></div>
     </section>`
   );
 };
+
+
+export {createFilmsListExtraTemplate};
