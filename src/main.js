26799502bf1fa22b9aef0f1f2697ba5af0340878
Анна Кузнецова <akimova_anna_25@mail.ru>
2020-04-26 17:42:41 +0300

    Переносим организацию 'filmsListsExtra' в 'PageController'.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import Filter from "./components/filter.js";
-import FilmsListExtra from "./components/films-list-extra.js";
+//import FilmsListExtra from "./components/films-list-extra.js";
 import PageController from "./controllers/page-controller.js";
 import {generateFilms} from "./mock/film";
 
 
-const FILM_COUNT = 23;
+const FILM_COUNT = 12;
 
 
 const films = generateFilms(FILM_COUNT);
@@ -16,11 +16,11 @@ const filmsFilters = {
   favorites: new Filter(`Favorites`, `filterFavoritesFun`, films),
 };
 
-const filmsListsExtra = [
+/* const filmsListsExtra = [
   new FilmsListExtra(`Top rated`, `rating`),
   new FilmsListExtra(`Most commented`, `commentsCount`),
-];
+]; */
 
 
-const pageController = new PageController(films, filmsFilters, filmsListsExtra);
+const pageController = new PageController(films, filmsFilters/* , filmsListsExtra */);
 pageController.render();
