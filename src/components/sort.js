15399cd562aba5c9b22faea6669ecd3a5e8871f8
Анна Кузнецова <akimova_anna_25@mail.ru>
2020-04-26 12:11:32 +0300

    Описываем типы сортировки в енуме. Добавляем в разметку data-set'ы с типом сортировки.

--- src/components/sort.js
+++ src/components/sort.js
@@ -1,13 +1,19 @@
 import AbstractComponent from "./abstract-component.js";
 
+const SortType = {
+  DEFAULT: `default`,
+  BY_DATE: `by-date`,
+  BY_RATING: `by-rating`,
+};
+
 export default class Sort extends AbstractComponent {
 
   getTemplate() {
     return (
       `<ul class="sort">
-        <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
-        <li><a href="#" class="sort__button">Sort by date</a></li>
-        <li><a href="#" class="sort__button">Sort by rating</a></li>
+        <li><a href="#" class="sort__button sort__button--active data-sort-type="${SortType.DEFAULT}">Sort by default</a></li>
+        <li><a href="#" class="sort__button" data-sort-type="${SortType.BY_DATE}">Sort by date</a></li>
+        <li><a href="#" class="sort__button" data-sort-type="${SortType.BY_RATING}">Sort by rating</a></li>
       </ul>`
     );
   }
