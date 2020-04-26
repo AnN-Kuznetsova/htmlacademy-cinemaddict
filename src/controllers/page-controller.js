bbf299e4c850cc3d8ccda4ce8146c949e843bda8
Анна Кузнецова <akimova_anna_25@mail.ru>
2020-04-26 14:01:27 +0300

    Переносим рендеоринг страницы из 'main.js' в 'PageController'.

--- src/controllers/page-controller.js
+++ src/controllers/page-controller.js
@@ -1,7 +1,39 @@
+import UserRank from "../components/user-rank.js";
+import SiteMenu from "../components/site-menu.js";
+import Filters from "../components/filters.js";
+import Sort from "../components/sort.js";
+import FilmsBoard from "../components/films-board.js";
+import FooterStatistics from "../components/footer-statistics.js";
+import FilmsBoardController from "./films-board-controller.js";
+import {render, RenderPosition} from "../utils/render.js";
+
+
+const bodyElement = document.querySelector(`body`);
+const siteHeaderElement = bodyElement.querySelector(`.header`);
+const siteMainElement = bodyElement.querySelector(`.main`);
+const siteFooterElement = bodyElement.querySelector(`.footer`);
+const footerStatisticsElement = siteFooterElement.querySelector(`.footer__statistics`);
+
 
 export default class PageController {
+  constructor(films, filmsFilters, filmsListsExtra) {
+    this._films = films;
+    this._filmsFilters = filmsFilters;
+    this._filmsListsExtra = filmsListsExtra;
+  }
 
   render() {
+    const siteMenuComponent = new SiteMenu();
+    render(siteHeaderElement, new UserRank(this._filmsFilters), RenderPosition.BEFOREEND);
+    render(siteMainElement, siteMenuComponent, RenderPosition.BEFOREEND);
+    render(siteMenuComponent.getElement(), new Filters(this._filmsFilters), RenderPosition.AFTERBEGIN);
+    render(siteMainElement, new Sort(), RenderPosition.BEFOREEND);
+    render(footerStatisticsElement, new FooterStatistics(this._films.length), RenderPosition.BEFOREEND);
 
+    const filmsBoardComponent = new FilmsBoard();
+    const filmsBoardController = new FilmsBoardController(filmsBoardComponent);
+
+    render(siteMainElement, filmsBoardComponent, RenderPosition.BEFOREEND);
+    filmsBoardController.render(this._films, this._filmsListsExtra);
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,23 +1,10 @@
-import UserRank from "./components/user-rank.js";
-import SiteMenu from "./components/site-menu.js";
 import Filter from "./components/filter.js";
-import Filters from "./components/filters.js";
-import Sort from "./components/sort.js";
-import FilmsBoard from "./components/films-board.js";
 import FilmsListExtra from "./components/films-list-extra.js";
-import FooterStatistics from "./components/footer-statistics.js";
-import FilmsBoardController from "./controllers/films-board-controller.js";
-import {render, RenderPosition} from "./utils/render.js";
+import PageController from "./controllers/page-controller.js";
 import {generateFilms} from "./mock/film";
 
 
 const FILM_COUNT = 23;
-
-const bodyElement = document.querySelector(`body`);
-const siteHeaderElement = bodyElement.querySelector(`.header`);
-const siteMainElement = bodyElement.querySelector(`.main`);
-const siteFooterElement = bodyElement.querySelector(`.footer`);
-const footerStatisticsElement = siteFooterElement.querySelector(`.footer__statistics`);
 
 
 const films = generateFilms(FILM_COUNT);
@@ -35,15 +22,5 @@ const filmsListsExtra = [
 ];
 
 
-const siteMenuComponent = new SiteMenu();
-render(siteHeaderElement, new UserRank(filmsFilters), RenderPosition.BEFOREEND);
-render(siteMainElement, siteMenuComponent, RenderPosition.BEFOREEND);
-render(siteMenuComponent.getElement(), new Filters(filmsFilters), RenderPosition.AFTERBEGIN);
-render(siteMainElement, new Sort(), RenderPosition.BEFOREEND);
-render(footerStatisticsElement, new FooterStatistics(films.length), RenderPosition.BEFOREEND);
-
-const filmsBoardComponent = new FilmsBoard();
-const pageController = new FilmsBoardController(filmsBoardComponent);
-
-render(siteMainElement, filmsBoardComponent, RenderPosition.BEFOREEND);
-pageController.render(films, filmsListsExtra);
+const pageController = new PageController(films, filmsFilters, filmsListsExtra);
+pageController.render();
