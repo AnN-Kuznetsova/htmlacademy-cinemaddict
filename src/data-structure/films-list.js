6973a85fd6f1793a1e6f42ebc4c426d0b75e84a0
Анна Кузнецова <akimova_anna_25@mail.ru>
2020-05-07 00:04:03 +0300

    Рефакторинг - 2.

--- src/data-structure/films-list.js
+++ src/data-structure/films-list.js
@@ -1,7 +1,19 @@
 export default class FilmsList {
   constructor(title, sortType, isExtra = false) {
-    this.title = title;
-    this.sortType = sortType;
-    this.isExtra = isExtra;
+    this._title = title;
+    this._sortType = sortType;
+    this._isExtra = isExtra;
+  }
+
+  get title() {
+    return this._title;
+  }
+
+  get sortType() {
+    return this._sortType;
+  }
+
+  get isExtra() {
+    return this._isExtra;
   }
 }
