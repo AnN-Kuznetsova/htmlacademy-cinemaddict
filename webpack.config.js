ddba931817d907744b9941ee0f5c23d7f7410c1f
Анна Кузнецова <akimova_anna_25@mail.ru>
2020-05-08 14:22:11 +0300

    Устанавливаем и подключаем библиотеку 'moment'.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require(`path`);
+const MomentLocalesPlugin = require(`moment-locales-webpack-plugin`);
 
 module.exports = {
   mode: `development`,
@@ -11,5 +12,10 @@ module.exports = {
   devServer: {
     contentBase: path.join(__dirname, `public`),
     watchContentBase: true
-  }
+  },
+  plugins: [
+    new MomentLocalesPlugin({
+      localesToKeep: [`es-us`],
+    })
+  ]
 };
