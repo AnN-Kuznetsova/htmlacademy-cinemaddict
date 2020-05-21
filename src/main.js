b5a02afdf9abb3b82af828176684d120cb0b5831
Анна Кузнецова <akimova_anna_25@mail.ru>
2020-05-22 00:37:33 +0300

    Добавили реализацую методов 'getItems' и 'setItem' в 'store'.

--- src/main.js
+++ src/main.js
@@ -5,8 +5,13 @@ import PageController from "./controllers/page-controller.js";
 import Store from "./api/store.js";
 
 
+const FILMS_STORE_PREFIX = `cinemaddict-localstorage`;
+const FILMS_STORE_VER = `v1`;
+const FILMS_STORE_NAME = `${FILMS_STORE_PREFIX}-${FILMS_STORE_VER}`;
+
+
 const filmsApi = new FilmsAPI();
-const filmsStore = new Store();
+const filmsStore = new Store(FILMS_STORE_NAME, window.localStorage);
 const filmsApiWithProvider = new FilmsProvider(filmsApi, filmsStore);
 const filmsModel = new FilmsModel();
 
